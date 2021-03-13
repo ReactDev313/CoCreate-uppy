aa48e177a0c67e21ef9bea2f480ede001054e676
Frank Pagan <frank@cocreate.app>
2021-03-13 02:36:47 +0000

    clean tree

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const AwsS3 = require('@uppy/aws-s3')
+// const AwsS3 = require('@uppy/aws-s3')
 const Uppy = require('@uppy/core')
 const Dashboard = require('@uppy/dashboard')
 require('@uppy/core/dist/style.css')
@@ -8,11 +8,11 @@ const uppy = new Uppy()
     .use(Dashboard, {
         trigger: '#select-files'
     })
-    .use(AwsS3, {
-        limit: 2,
-        timeout: 1000 * 60,
-        companionUrl: 'https://uppy-companion.myapp.com/'
-    })
+    // .use(AwsS3, {
+    //     limit: 2,
+    //     timeout: 1000 * 60,
+    //     companionUrl: 'https://uppy-companion.myapp.com/'
+    // })
 
 uppy.on('complete', (result) => {
     console.log('Upload complete! We’ve uploaded these files:', result.successful)
