8fa94a0e73996a268c6ee922162e71a92ae7a901
ReactDev313 <reactist313@hotmail.com>
2021-03-14 09:32:08 -0600

    image editor with pencil icon when upload image

--- src/index.js
+++ src/index.js
@@ -2,17 +2,19 @@
 const Uppy = require("@uppy/core");
 const XHRUpload = require("@uppy/xhr-upload");
 const Dashboard = require("@uppy/dashboard");
+const ImageEditor = require("@uppy/image-editor");
 
 // And their styles (for UI plugins)
 // With webpack and `style-loader`, you can require them like this:
 require("@uppy/core/dist/style.css");
 require("@uppy/dashboard/dist/style.css");
-
+require("@uppy/image-editor/dist/style.css");
 const uppy = new Uppy()
   .use(Dashboard, {
     inline: true,
     trigger: "#drag-drop-area",
   })
+  .use(ImageEditor, { target: Dashboard })
   .use(XHRUpload, { endpoint: "https://api2.transloadit.com" });
 
 uppy.on("file-added", (file) => {
